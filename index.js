ccca2eb4c29964c67d1da9ec35864f1526d36b29
Ric Lucas <98325951+riclpicl@users.noreply.github.com>
2022-04-01 09:06:08 -0500

    Add response for / route.

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+const exp = require('constants');
 const express = require('express');
 const path = require('path');
 const PORT = process.env.PORT || 5000;
@@ -9,3 +10,23 @@ const pool = new Pool({
     rejectUnauthorized: false
   }
 });
+
+express()
+.use(express.static(path.join(__dirname, 'public')))
+.use(express.json())
+.use(express.urlencoded({extended: true}))
+.set('views', path.join(__dirname, 'views'))
+.set('view engine', 'ejs')
+.get('/', async(req, res) => {
+  try {
+  const client = await pool.connect();
+
+  client.release();
+  res.send("Works");
+  }
+  catch (err) {
+    console.error(err);
+    res.send("Error " + err);
+  }
+})
+.listen(PORT, () => console.log("Listening on ${ PORT }"));
